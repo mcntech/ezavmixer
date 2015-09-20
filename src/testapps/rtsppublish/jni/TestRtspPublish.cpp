1c80aa8149ff11da053c6bae08fcdc2cce83906e
rampenke@mcntech.com <rampenke@mcntech.com>
2015-09-20 11:36:27 -0700

    Test rtsp publish sanppshot

--- src/testapps/rtsppublish/jni/TestRtspPublish.cpp
+++ src/testapps/rtsppublish/jni/TestRtspPublish.cpp
@@ -38,9 +38,33 @@
 
 CRtspPublishConfig::CRtspPublishConfig(const char *pszConfFile)
 {
+	/*
 	ini_gets(SECTION_RTSP_PUBLISH, KEY_RTSP_PUBLISH_HOST, "", szRtspServerAddr, 64, pszConfFile);
 	ini_gets(SECTION_RTSP_PUBLISH, KEY_RTSP_PUBLISH_APPLICATION, "", szApplicationName, 64, pszConfFile);
 	usRtpLocalPort =  (unsigned short)ini_getl(SECTION_RTSP_PUBLISH, "local_port",   59500, pszConfFile);
 	usRtpRemotePort =  (unsigned short)ini_getl(SECTION_RTSP_PUBLISH, "remote_port",   49500, pszConfFile);
 	usServerRtspPort =  (unsigned short)ini_getl(SECTION_RTSP_PUBLISH, KEY_RTSP_PUBLISH_RTSP_PORT,   1935, pszConfFile);
+	*/
+}
+CRtspPublishBridge  *m_pRtspSrvBridge;
+int main()
+{
+	m_pRtspSrvBridge = new CRtspPublishBridge;
+	m_pRtspCommonCfg = new CRtspCommonConfig(ONYX_PUBLISH_FILE_NAME);
+
+	m_pRtspSrvBridge->SetStreamCfg(m_pRtspCommonCfg);
+	m_pRtspSrvBridge->Init(m_pOutputStream);
+
+	if(m_fEnableRtspSrv) {
+		m_pRtspSrvBridge->SetRtspServerCfg(m_pRtspSvrCfg);
+		m_pRtspSrvBridge->StartRtspServer();
+	}
+	if(m_fEnableRtspPublish) {
+		m_pRtspSrvBridge->SetPublishServerCfg(m_pRtspPublishCfg);
+		m_pRtspSrvBridge->ConnectToPublishServer();
+	}
+
+	if(m_pRtspSrvBridge)
+		pPublishSwitch->AddOutput(m_pRtspSrvBridge);
+
 }

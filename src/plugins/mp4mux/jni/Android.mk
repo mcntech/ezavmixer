ed66323f2a06fb2a31ddc4d80871d3295fe6946a
mcntech <rampenke@mcntech.com>
2015-10-18 22:14:27 -0700

    new file:   ../../plugins/mp4mux/jni/Android.mk 	new file:   ../../plugins/mp4mux/jni/Application.mk 	new file:   ../../plugins/parseutil/include/TsParse.h 	new file:   ../../plugins/parseutil/include/h264parser.h 	new file:   ../../plugins/parseutil/jni/Android.mk 	new file:   ../../plugins/parseutil/jni/Application.mk 	new file:   ../../plugins/parseutil/src/h264parser.cpp

---
+++ src/plugins/mp4mux/jni/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := mp4mux
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/.. 
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include 
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../parseutil/include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../logutil/include
+
+MY_SRCS := ../src
+
+LOCAL_CFLAGS += -DPLATFORM_ANDROID
+
+LOCAL_SRC_FILES := $(MY_SRCS)/Mp4Mux.cpp
+
+include $(BUILD_STATIC_LIBRARY)

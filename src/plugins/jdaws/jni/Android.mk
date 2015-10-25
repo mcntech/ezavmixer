28b62df84eeed24bc589e2aaf4eba72d0d3cf3e9
rampenke@mcntech.com <rampenke@mcntech.com>
2015-10-25 14:53:54 -0700

    new file:   ../../plugins/jdaws/jni/Android.mk  	new file:   ../../plugins/jdaws/jni/Application.mk  	new file:   ../../plugins/jdaws/src/SegmentWriteS3.cpp  	new file:   ../../plugins/streamif/include/SegmentWriteBase.h

---
+++ src/plugins/jdaws/jni/Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := jdaws
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+JD_PLUGIN_FOLDER := $(LOCAL_PATH)/../../../plugins
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/.. 
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../logutil/include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../extlibs/openssl-1.0.2/include
+LOCAL_C_INCLUDES += $(JD_PLUGIN_FOLDER)/streamif/include
+
+MY_SRCS := ../src
+
+LOCAL_CFLAGS += -DPLATFORM_ANDROID
+
+LOCAL_SRC_FILES := $(MY_SRCS)/JdAwsConfig.cpp
+LOCAL_SRC_FILES += $(MY_SRCS)/JdAwsContext.cpp
+LOCAL_SRC_FILES += $(MY_SRCS)/JdAwsS3.cpp
+LOCAL_SRC_FILES += $(MY_SRCS)/SegmentWriteS3.cpp
+LOCAL_SRC_FILES += $(MY_SRCS)/JdAwsS3UpnpHttpConnection.cpp
+
+include $(BUILD_STATIC_LIBRARY)
---
+++ src/plugins/jdaws/src/SegmentWriteS3.cpp
@@ -0,0 +1,184 @@
+
+#include "../include/SegmentWriteS3.h"
+
+CSegmentWriteS3::CSegmentWriteS3(
+			const char *pszBucket,
+			const char *pszHost,
+			const char *pszAccessId,
+			const char *pszSecKey)
+		: m_AwsContext(pszAccessId, pszSecKey, NULL, pszHost)
+{
+	m_Bucket.assign(pszBucket);
+}
+
+
+int CSegmentWriteS3::Start(
+			const char *pszParent,
+			const char *pszFile,
+			int        nTotalLen,	/* Totoal length of content including subsequent continue */
+			char       *pData,
+			int        nLen,		/* Length of data of this call */
+			const char *pContentType
+			)
+{
+    int    httpStatus;
+	int nTimeOut = 30;
+	size_t size = nLen;
+	CJdAwsS3Request request(&m_AwsContext);
+
+	request.methodM = CJdAwsS3Request::PUT;
+
+	if(pContentType != NULL && strlen(pContentType)){
+		request.contentTypeM.assign(pContentType);
+	} else {
+		request.contentTypeM.assign(CONTENT_STR_DEF);
+	}
+
+	if (pszParent) {
+		std::ostringstream objectname;
+		objectname << "/" << pszParent << "/" << pszFile;
+		request.pathM.assign(objectname.str());
+	} else {
+		request.pathM.assign(pszFile);
+	}
+	request.bucketNameM.assign(m_Bucket);
+
+	request.contentLengthM = nTotalLen;
+	request.fUseHttpsM = false; //gUseHttps;
+
+	CJdAwsContext::GetCurrentDate(request.dateM);
+
+	JD_STATUS status = CJdAwsS3HttpConnection::MakeRequest(request, m_SessionContext);
+	if(status != JD_OK) goto Exit;
+
+	if(pData) {
+		status = JdAwsWriteHttpRequest(m_SessionContext.pHandleM,  pData, &size, 30);
+	}
+
+Exit:
+    return status;
+}
+
+int CSegmentWriteS3::Continue(char *pData, int nLen)
+{
+    int			httpStatus;
+	JD_STATUS	status;
+	int nTimeOut = 30;
+	size_t size = nLen;
+    status = JdAwsWriteHttpRequest(m_SessionContext.pHandleM,  pData, &size, 30);
+	return (status == JD_OK);
+}
+
+int CSegmentWriteS3::End(char *pData, int nLen)
+{
+    int			httpStatus;
+	JD_STATUS	status;
+	int nTimeOut = 30;
+	size_t size = nLen;
+	if(pData && nLen > 0) {
+		status = JdAwsWriteHttpRequest(m_SessionContext.pHandleM,  pData, &size, 30);
+		if(status != JD_OK) goto Exit;
+	}
+    status = JdAwsEndHttpRequest(m_SessionContext.pHandleM, nTimeOut);
+	if(status != JD_OK) goto Exit;
+    status = JdAwsGetHttpResponse(m_SessionContext.pHandleM, NULL, NULL, NULL,
+                                     &httpStatus, nTimeOut);
+	JdAwsCloseHttpConnection(m_SessionContext.pHandleM);
+	m_SessionContext.pHandleM = 0;
+
+Exit:
+    return(status == JD_OK);
+}
+
+int CSegmentWriteS3::Send(const char *pszParent, const char *pszFile, char *pData, int nLen, const char *pContentType, int nTimeOut)
+{
+	int result = JD_OK;
+    int httpStatus;
+	size_t size = nLen;
+	//return HttpPutResource(pszParent, pszFile, pData, nMaxLen, nTimeOut, CONTENT_TYPE_HTML, AMZ_ACL_PUBLIC_READ_WRITE);
+
+	CJdAwsS3Request request(&m_AwsContext);
+
+	request.methodM = CJdAwsS3Request::PUT;
+	request.pContextM = &m_AwsContext;
+	request.hostM.assign("s3.amazonaws.com");
+
+	if(pContentType != NULL && strlen(pContentType)){
+		request.contentTypeM.assign(pContentType);
+	} else {
+		request.contentTypeM.assign(CONTENT_STR_DEF);
+	}
+
+	if (pszParent) {
+		std::ostringstream objectname;
+		objectname << "/" << pszParent << "/" << pszFile;
+		request.pathM.assign(objectname.str());
+	} else {
+		request.pathM.assign(pszFile);
+	}
+	request.bucketNameM.assign(m_Bucket);
+
+	request.contentLengthM = size;
+	request.fUseHttpsM = false; //gUseHttps;
+
+	CJdAwsContext::GetCurrentDate(request.dateM);
+
+	CJdAwsS3HttpResponse response;
+	JD_STATUS status = CJdAwsS3HttpConnection::MakeRequest(request, response);
+    if(status != JD_OK){
+		result = JD_ERROR_CONNECTION;
+		goto Exit;
+	}
+    assert(response.pHandleM);
+    status = JdAwsWriteHttpRequest(response.pHandleM,  pData, &size, 30);
+    if(status != JD_OK){
+		result = JD_ERROR_PUT_WRITE;
+		goto Exit;
+	}
+    status = JdAwsEndHttpRequest(response.pHandleM, nTimeOut);
+    assert(status == JD_OK);
+    status = JdAwsGetHttpResponse(response.pHandleM, NULL, NULL, NULL,
+                                     &httpStatus, nTimeOut);
+
+    if(status != JD_OK){
+		result = JD_ERROR_PUT_RESPONSE;
+		goto Exit;
+	}
+    if(httpStatus != 200){
+		result = JD_ERROR_PUT_REJECT;
+	}
+Exit:
+	return result;
+}
+
+int CSegmentWriteS3::Delete(const char *pszParent, const char *pszFile)
+{
+    int    httpStatus;
+	int nTimeOut = 30;
+	CJdAwsS3Request request(&m_AwsContext);
+
+	request.methodM = CJdAwsS3Request::REMOVE;
+
+	if (pszParent) {
+		std::ostringstream objectname;
+		objectname << "/" << pszParent << "/" << pszFile;
+		request.pathM.assign(objectname.str());
+	} else {
+		request.pathM.assign(pszFile);
+	}
+	request.bucketNameM.assign(m_Bucket);
+
+	request.fUseHttpsM = false; //gUseHttps;
+
+	CJdAwsContext::GetCurrentDate(request.dateM);
+
+	JD_STATUS status = CJdAwsS3HttpConnection::MakeRequest(request, m_SessionContext);
+
+	JdAwsCloseHttpConnection(m_SessionContext.pHandleM);
+	m_SessionContext.pHandleM = 0;
+
+	if(status != JD_OK) goto Exit;
+
+Exit:
+    return status;
+}
